1dffcb5f6c2ffdd3126f133f674477838073bae6
Евгений Аборкин <newtowngenek@gmail.com>
2023-06-11 23:22:45 +0300

    Добавление модели User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+ROLE_TYPE = ['user', 'moderator', 'admin']
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        null=True,
+        blank=True,
+    )
+    role = models.CharField(
+        'Роль',
+        max_length=10,
+        choices=ROLE_TYPE,
+        default='user',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_username_email'
+            )
+        ]
