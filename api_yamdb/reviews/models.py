840059950e1b071de47f1fa8f810f377c3de4bd6
Gregory Shevchuk <2097255@mail.ru>
2023-06-12 11:16:50 +0300

    Созданы модели Title,Category, Genre, добавлен путь к шаблонам.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField
+    rating = models.IntegerField
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre,
+        blank=True,
+        null=True,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+    )
