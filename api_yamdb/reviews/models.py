174cacb32b4fde520da0b8e2d4f6c14c173fce78
Евгений Аборкин <newtowngenek@gmail.com>
2023-06-12 12:06:21 +0300

    Исправление ROLE_TYPE

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,11 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    ROLE_TYPE = [
-        ('user', 'User'),
-        ('moderator', 'Moderator'),
-        ('admin', 'Admin'),
-    ]
+    ROLE_TYPE = ['user', 'moderator', 'admin']
 
     bio = models.TextField(
         'Биография',
