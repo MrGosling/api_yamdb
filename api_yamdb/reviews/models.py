70e9c4a0606fbf446e2b193c524496bab14cb419
shevchukgr <120850661+shevchukgr@users.noreply.github.com>
2023-06-12 20:38:12 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,14 +3,6 @@ from django.db import models
 from reviews.base_models import BaseModel
 
 
-class Genre(BaseModel):
-    slug = models.SlugField(unique=True)
-
-
-class Category(BaseModel):
-    slug = models.SlugField(unique=True)
-
-
 class CustomUser(AbstractUser):
     ROLE_TYPE = [
         ('user', 'user'),
@@ -41,6 +33,15 @@ class CustomUser(AbstractUser):
                 name='unique_email'
             )
         ]
+
+
+class Genre(BaseModel):
+    slug = models.SlugField(unique=True)
+
+
+class Category(BaseModel):
+    slug = models.SlugField(unique=True)
+
 
 
 class Title(BaseModel):
