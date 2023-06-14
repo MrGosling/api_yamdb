6907d8f502eaa61248fc269cd61a0493f5268c13
Genek91 <121492431+Genek91@users.noreply.github.com>
2023-06-14 21:51:11 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=14),
 }
 
 # Модуль для сохранения писем в файлы
