bb6b04fa3386a08f2ad2a7d34459a0f4ee59bff3
Евгений Аборкин <newtowngenek@gmail.com>
2023-06-14 14:35:47 +0300

    UserViewSet

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,18 +2,15 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from api.views import AdminUserViewSet, UserViewSet
+from api.views import UserViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
-#router.register('users/me', UserViewSet, basename='users_me')
-router.register('users', AdminUserViewSet, basename='users')
-router.register('users/<str:username>', AdminUserViewSet, basename='users')
+router.register('users', UserViewSet, basename='users')
 
 
 urlpatterns = [
-    path('v1/users/me/', UserViewSet.as_view({'get': 'list', 'patch': 'partial_update'}), name='users_me'),
     #path('v1/auth/signup/', ),
     path('v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_view'),
     path('v1/', include(router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
 from api.serializers import UserSerializer, PartialUserSerializer
@@ -8,38 +7,26 @@ from api.permissions import AdminPermission, UserPermission
 from reviews.models import CustomUser
 
 
-class AdminUserViewSet(ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AdminPermission]
-    lookup_field = 'username'
+    lookup_field = "username"
 
-    def retrieve(self, *args, **kwargs):
-        user = get_object_or_404(
-            self.queryset, username=self.kwargs.get('username')
+    @action(
+        detail=True,
+        methods=['get', 'patch'],
+        permission_classes=[UserPermission]
+    )
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = PartialUserSerializer(request.user)
+            return Response(serializer.data)
+
+        user = request.user
+        serializer = PartialUserSerializer(
+            user, data=request.data, partial=True
         )
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-
-class UserViewSet(ModelViewSet):
-    serializer_class = PartialUserSerializer
-    permission_classes = [UserPermission]
-
-    def get_queryset(self):
-        return CustomUser.objects.filter(username=self.request.user.username)
-
-    def get_object(self):
-        return self.request.user
-
-    def list(self, request):
-        queryset = self.get_queryset()
-        serializer = UserSerializer(queryset.first())
-        return Response(serializer.data)
-
-    def partial_update(self, request, *args, **kwargs):
-        user = self.get_object()
-        serializer = PartialUserSerializer(user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
