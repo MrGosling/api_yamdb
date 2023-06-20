3d03d7750152505185d77a7daa434e43a857c2e5
skofanov <kofan1106@gmail.com>
2023-06-20 22:23:09 +0300

    Создание моделей в админке и прочие правки

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,51 @@
 from django.contrib import admin
 
-from reviews.models import CustomUser
+from reviews.models import CustomUser, Category, Comment, Genre, Review, Title
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'role',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+    )
+    search_fields = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'pub_date', 'review')
+    empty_value_display = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'pub_date', 'title', 'score')
+    empty_value_display = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category', 'description')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Title, TitleAdmin)
